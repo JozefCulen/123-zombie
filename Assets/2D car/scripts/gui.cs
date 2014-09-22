27f54dc14d8066f54f343201ed6387c64e056e2b
Jozef Culen <info@pozadia.org>
2014-09-22 17:48:05 +0200

    prvy commit

---
+++ Assets/2D car/scripts/gui.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gui : MonoBehaviour {
+
+	void OnGUI () {
+		// Make a background box
+		//GUI.Box(new Rect(10,10,100,90), "Loader Menu");
+		GUI.Label(new Rect(10,10,100,90), "status");
+	}
+	public void changeText(string text){
+		GUI.Label(new Rect(10,10,100,90), text);
+	}
+}
