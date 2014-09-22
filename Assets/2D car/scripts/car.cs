092ee830f0694851daf218c846abdab9cbdf7b64
Jozef Culen <info@pozadia.org>
2014-09-22 19:15:37 +0200

    bug fix ovladani a zmenen auto

--- Assets/2D car/scripts/car.cs
+++ Assets/2D car/scripts/car.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class car : MonoBehaviour {
 	WheelJoint2D kolesoZ ;
 	Camera cam;
-	int maxMotorSpeed = 2000;
-	int acceleration = 100;
-	int dec = 20;
+	 int maxMotorSpeed = 2000;
+	int acceleration = 500;
+
 	// Use this for initialization
 	void Start () {
 		kolesoZ = GetComponent<WheelJoint2D>();
@@ -18,25 +18,34 @@ public class car : MonoBehaviour {
 	void Update () {
 
 		JointMotor2D mot = kolesoZ.motor;
+		if (Input.anyKeyDown) {
+			//Debug.Log("stalcam");
+			Debug.Log(mot.motorSpeed);
+		}
 		if (Input.GetKey(KeyCode.LeftArrow)) {
-
 			if (mot.motorSpeed <= maxMotorSpeed){
 				kolesoZ.useMotor = true;
-				mot.motorSpeed += acceleration;
+				if(mot.motorSpeed >= maxMotorSpeed )
+					mot.motorSpeed = maxMotorSpeed;
+				else
+					mot.motorSpeed += acceleration;
 				kolesoZ.motor = mot;
 			}
 		}
 		else if (Input.GetKey(KeyCode.RightArrow)) {
 			if (mot.motorSpeed >= -maxMotorSpeed){
 				kolesoZ.useMotor = true;
-				mot.motorSpeed += -acceleration;
+				if(mot.motorSpeed <= -maxMotorSpeed )
+					mot.motorSpeed = -maxMotorSpeed;
+				else
+					mot.motorSpeed -= acceleration;
 				kolesoZ.motor = mot;
 			}
 		}
 		else{
 			if(kolesoZ.useMotor == true){
 				kolesoZ.useMotor = false;
-			Debug.Log ("motor disabled");
+			//Debug.Log ("motor disabled");
 			}
 		}
 	}
