7c6d33b4613ffe7c356fdf7ac2425e05d174677d
flashire <blanarik.martin@gmail.com>
2014-11-02 18:55:54 +0100

    menu
    js -> c#

---
+++ Assets/Menu/scripts/MenuMouseHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMouseHandler : MonoBehaviour {
+	protected string level = "APC-test";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		Application.LoadLevel(this.level);
+	}
+}
--- Assets/Menu/scripts/MenuMouseHandler.js
+++
@@ -1,9 +0,0 @@
-﻿var level = "APC-test";
-
-function OnMouseDown()
-{
-    if (this.name == "btn_Play")
-    {
-        Application.LoadLevel(level);
-    }
-}
